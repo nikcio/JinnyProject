2069b7f0f0f2537a37d42be8634290dfa462bf7f
Nikcio <nikolajgive@gmail.com>
2020-04-12 00:15:08 +0200

    Initial commit

---
+++ Frontend-Dev/gulp-config.js
@@ -0,0 +1,57 @@
+// Modify values below to change file structure and/or selection of files
+var root = 'src/',	
+	sass = root + "sass/**/*.scss",
+	js = root + "js/**/*.js",
+	images = root + 'assets/img/**/*',
+	video = root + 'assets/video/**/*',
+	svg =  root + 'assets/svg/*.svg',
+	fonts = root + 'assets/fonts/**/*',
+
+
+	dist = 'dist/',	
+	distCSS = dist + 'css/',
+	distJS = dist + 'scripts/',
+	distImages = dist + 'img/',
+	distVideo = dist + 'video/',
+	distSVG = dist + 'svg/',
+	distFonts = dist + 'fonts/',
+
+
+	injectSourceCSS = distCSS + '/**/*.css',
+	injectSourceJS = distJS + '/**/*.js',
+	watchHTML = root + '**/*.html';
+
+// These settings change how the output of gulp are processed
+// isBuild will determine if css and js will be minified
+// createSourcemaps will determine if sassFormat will create sourcemaps
+const settings = {
+	isBuild: false,
+	createSourcemaps: false
+};
+	
+
+module.exports  = {
+	base: root,	
+	sass: sass,
+	js: js,
+	images: images,
+	video: video,
+	svg:  svg,
+	fonts: fonts,
+
+
+	dist: dist,	
+	distCSS: distCSS,
+	distJS: distJS,
+	distImages: distImages,
+	distVideo: distVideo,
+	distSVG: distSVG,
+	distFonts: distFonts,
+
+
+	injectSourceCSS: injectSourceCSS,
+	injectSourceJS: injectSourceJS,
+	watchHTML: watchHTML,
+
+	settings: settings,
+}
---
+++ Frontend-Dev/gulpfile.js
@@ -0,0 +1,219 @@
+/*
+* variables ===================================================================================== 
+*/
+
+//SVG processing
+const svgmin = require('gulp-svgmin');
+const svgstore = require('gulp-svgstore');
+const cheerio = require('gulp-cheerio');
+
+//Sass processing
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+//Javascript processing
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+//Image processing
+const imagemin = require('gulp-imagemin');
+
+//Inject processing
+const inject = require('gulp-inject');
+const fileInclude = require('gulp-file-include');
+
+// Gulp + gulp tools
+const gulp = require('gulp');
+const del = require('del');
+const gulpif = require('gulp-if');
+const rename = require("gulp-rename");
+
+// Browsersync
+const browserSync = require('browser-sync');
+
+// Paths
+const config = require('./gulp-config');
+
+// Select the html files to inject css and js into
+const pagesToInject = [
+    "/html/index.html"
+];
+
+const basePages = [];
+
+pagesToInject.forEach(function(element) {
+	basePages.push(config.base + element);
+});
+
+/**
+ * Tasks =========================================================================================
+ */
+
+
+//
+// SVG
+// --------------------------------------------------
+function svg() {
+	return gulp.src(config.svg)
+		.pipe(svgmin(function(file) { // Minify and clean up svg files
+			const prefix = path.basename(file.relative, path.extname(file.relative));
+			return {
+				plugins: [
+					{ cleanupIDs: { prefix: prefix + "-", minify: true } },
+					{ removeDoctype: true },
+					{ removeComments: true },
+					{ cleanupNumericValues: { floatPrecision: 2 } },
+					{ removeAttrs: { attrs: '(fill|stroke)' } }
+				]
+			}
+		}) )
+		.pipe(svgstore({ inlineSvg: true })) // Combine into 1 sprite sheet
+		.pipe(cheerio(function($) { // Modify resulting <svg> element
+			$('svg').attr('style', 'display:none');
+		}))
+		.pipe(rename('combined.svg'))
+		.pipe(gulp.dest( config.distSVG))
+};
+
+
+//
+// IMAGES
+// --------------------------------------------------
+function images() {
+	return gulp.src(config.images)
+		.pipe(imagemin({
+			progressive: true,
+			interlaced: true
+		}))
+		.pipe(gulp.dest(config.distImages))
+};
+
+
+//
+// VIDEO
+// --------------------------------------------------
+function video() {
+    return gulp.src(config.video)
+        .pipe(gulp.dest(config.distVideo))
+};
+
+
+//
+// FONTS
+// --------------------------------------------------
+function fonts() {
+	return gulp.src(config.fonts)
+		.pipe(gulp.dest(config.dist))
+};
+
+
+//
+// INJECT STYLES AND HTML
+// --------------------------------------------------
+function injectToHTML() {
+
+	let sources = [
+		config.injectSourceCSS, config.injectSourceJS
+	];
+
+	return gulp.src(basePages)
+		.pipe(inject(gulp.src(sources, {read: false}), {
+			addRootSlash: false,
+			ignorePath: config.dist
+		}))
+		.pipe(fileInclude().on('error', function() {
+			console.log(arguments);
+		}))
+		.pipe( gulp.dest( config.dist ) )
+};
+
+
+//
+// SASS - auto-prefix and minify
+// Uses browserslist from package.json
+// --------------------------------------------------
+function sassFormat() {
+	return gulp.src(config.sass)
+		.pipe(gulpif(config.settings.createSourcemaps, sourcemaps.init()))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulpif(config.settings.isBuild, cleanCSS({compatibility: 'ie8'})))
+		.pipe(gulpif(config.settings.createSourcemaps, sourcemaps.write('./maps')))
+		.pipe(gulp.dest(config.distCSS))
+};
+
+
+//
+// JAVASCRIPT - Concat and uglify
+// --------------------------------------------------
+function JSFormat() {
+	return gulp.src(config.js)
+		.pipe(concat('combined.js'))
+		.pipe(gulpif(config.settings.isBuild, uglify()))
+		.pipe(gulp.dest(config.distJS))
+};
+
+
+//
+// CLEAN
+// --------------------------------------------------
+function clean() {
+	return del( config.dist + '*' );
+};
+
+
+//
+// SERVE
+// Set up BrowserSync server and host local dev site
+// --------------------------------------------------
+const server = browserSync.create();
+
+function reload(done) {
+	server.reload();
+	done();
+}
+
+function serve(done) {
+	server.init({
+		server: {
+			baseDir: config.dist
+			}
+	});
+	done();
+}
+
+function watch (){
+	gulp.watch(config.js, gulp.series(JSFormat, reload));
+	gulp.watch(config.sass, gulp.series(sassFormat));
+	gulp.watch(config.images, gulp.series(images));
+	gulp.watch(config.video, gulp.series(video));
+	gulp.watch(config.svg, gulp.series(svg));
+	gulp.watch(config.fonts, gulp.series(fonts));
+	gulp.watch(config.watchHTML, gulp.series(injectToHTML, reload));
+}
+
+
+//
+// TASK DEV
+// --------------------------------------------------
+const dev = gulp.series(clean, images, video, fonts, svg, sassFormat, JSFormat, injectToHTML, serve, watch);
+
+
+//
+// Exports
+// --------------------------------------------------
+exports.default = dev;
+exports.dev = dev;
+exports.clean = clean;
+exports.sassFormat = sassFormat;
+exports.images = images;
+exports.video = video;
+exports.fonts = fonts;
+exports.svg = svg;
+exports.JSFormat = JSFormat;
+exports.injectToHTML = injectToHTML;
+exports.serve = serve;
+exports.watch = watch;
+exports.reload = reload;
