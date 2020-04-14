030177ef696af2d2f80086102a3d0725a128ca7b
Nikcio <nikolajgive@gmail.com>
2020-04-14 13:04:35 +0200

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c#m7h&f^d$-f(%6sphaohkj^(0h(tq%6qdj^09=33bb1^-0z9l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["jinnytty.com", "www.jinnytty.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
