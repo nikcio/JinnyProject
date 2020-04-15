71afe9797315ca4b580c60f6ad511fb1237cee67
Nikcio <nikolajgive@gmail.com>
2020-04-15 03:30:39 +0200

    script update

--- static/jinny_cooking/js/get_streamer.js
+++ static/jinny_cooking/js/get_streamer.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
       const req = new XMLHttpRequest();
-      req.open("GET",'https://api.twitch.tv/helix/streams?user_id=159498717', true);
+      //req.open("GET",'https://api.twitch.tv/helix/streams?user_id=159498717', true);
+      req.open("GET",'https://api.twitch.tv/helix/streams?user_login=jinnytty', true);
       //req.open("GET",'https://api.twitch.tv/helix/streams?user_id=235634967', true); // TESTER Channel: yugwha0901
       req.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
       req.setRequestHeader('Client-ID', 'ijqkaxcdnlcq5j1xbndidaer460p7l');
@@ -8,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function(){
       req.onload = function(){
           const json = JSON.parse(req.responseText);
           if(json.data.length != 0){
-            //console.log(json.data.length);
+            //console.log(json);
             $('.live-now').addClass("show");
           }
       }
