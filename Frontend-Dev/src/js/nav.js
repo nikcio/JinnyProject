7985114f431e8ea6485c32908311f5b5c464a8cf
Nikcio <nikolajgive@gmail.com>
2020-04-16 22:37:35 +0200

    CSS improvements + cloapseable menu

---
+++ Frontend-Dev/src/js/nav.js
@@ -0,0 +1,10 @@
+function showMenu(){
+   $(".right").addClass("show");
+   $("body").addClass("show-menu")
+}
+
+
+function closeMenu(){
+    $(".right").removeClass("show");
+    $("body").removeClass("show-menu")
+}
