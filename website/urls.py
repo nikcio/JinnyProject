23d9da588c9b2a5816966e901930eccd5bb61cbf
Nikcio <nikolajgive@gmail.com>
2020-04-19 05:55:00 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cooking.urls')),
     # path('accounts', include('user_management.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
