7d9cc56033ec35ac0d7f62bd2a9f52a5ce321c92
Nikcio <nikolajgive@gmail.com>
2020-04-29 21:57:48 +0200

    General implementation
    Needs styling

--- cooking/urls.py
+++ cooking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.conf.urls import *
 from cooking import views as view
 
 urlpatterns = [
-    path('', view.Index.as_view()),
-    path('about/', view.About.as_view()),
-    path('recipes/', view.ViewAll.as_view()),
+    path('', view.Index.as_view(), name="homepage"),
+    path('about/', view.About.as_view(), name="about"),
+    path('recipes/', view.ViewAll.as_view(), name="recipes"),
     path('recipe/<slug:slug>', view.Detail.as_view(), name="recipe-detail"),
 ]
